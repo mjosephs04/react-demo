224d7871d825ad83c20fed4d1c6b4ecdaa64807a
markjosephs <mjosephs@customsportssleeves.com>
2024-03-08 15:52:44 -0500

    have the hover kinda working

--- src/modules/About/index.js
+++ src/modules/About/index.js
@@ -4,7 +4,6 @@ import Layout from "../Layout";
 const Test = () =>{
     return (
         <div>
-            <Layout />
             <h1>testing testing</h1>
             <h1>where is this??</h1>
         </div>
